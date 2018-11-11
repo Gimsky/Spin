27d0bd8a5be924d394d17f46996d40607e7160cb
Gimka <42877856+Gimsky@users.noreply.github.com>
2018-11-11 09:44:46 +0300

    Create

---
+++ js/scripts.js
@@ -0,0 +1,62 @@
+var canv = document.getElementById("canvas"),
+    ctx    = canv.getContext("2d"),
+    w = 600,
+    h = 600,
+    R = 180
+    r = 90,
+    d = 80,
+    teta = 0;
+var timer;  
+var t = 100;
+var myColor = 'lime';
+
+document.getElementById("Rvalue").textContent = 'R: ' + R;
+document.getElementById("rvalue").textContent = 'r: ' + r;
+document.getElementById("dvalue").textContent = 'd: ' + d;
+document.getElementById("tvalue").textContent = 't: ' + t;
+document.getElementById("t").value = t;
+
+document.getElementById("color").onchange = function(){
+  myColor = this.value;
+};
+
+document.getElementById("R").oninput = function(){
+  ctx.clearRect(0,0,w,h);
+  R = this.value;
+  document.getElementById("Rvalue").textContent = 'R: ' + R;
+};
+
+
+document.getElementById("r").oninput = function(){
+  ctx.clearRect(0,0,w,h);
+  r = this.value;
+  document.getElementById("rvalue").textContent = 'r: ' + r;
+};
+
+document.getElementById("d").oninput = function(){
+  ctx.clearRect(0,0,w,h);
+  d = this.value;
+  document.getElementById("dvalue").textContent = 'd: ' + d;
+};
+
+document.getElementById("t").oninput = function(){
+  ctx.clearRect(0,0,w,h);
+  t = this.value;
+  document.getElementById("tvalue").textContent = 't: ' + t;
+};
+
+
+canv.height = h;
+canv.width = w;
+function spiro(){
+  var x = (R - r) * Math.cos(teta) + d * Math.cos( (R - r) * teta/r );
+  var y = (R - r) * Math.sin(teta) - d * Math.sin( (R - r) * teta/r );
+  teta += 0.1;
+  ctx.fillStyle = myColor;
+  ctx.beginPath();
+  ctx.arc(300+x, 300+y, 4, 0, Math.PI * 2);
+  ctx.fill();
+  timer = setTimeout(spiro, t);
+}
+
+spiro();
